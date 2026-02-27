1ca77d2e5d79303d1e9201f787b68d770bcbf084
the-practice <57959512+the-practice@users.noreply.github.com>
2026-02-27 00:13:42 -0500

    Fix: Add get_db to database module exports

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -5,4 +5,4 @@ Database module.
 from app.database.session import engine, Base, get_db
 from app.database.init_db import init_db
 
-__all__ = ["engine", "Base", "init_db", "get_db"]
+__all__ = ["engine", "Base", "get_db", "init_db"]
